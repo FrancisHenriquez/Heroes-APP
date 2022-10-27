0142c47640f661d48b5dbdf121b73f6e8e3498a1
FrancisHenriquez <francishenriquez07@gmail.com>
2022-10-27 11:48:32 -0400

    boton de logout

--- src/UI/components/Navbar.jsx
+++ src/UI/components/Navbar.jsx
@@ -1,8 +1,18 @@
 import React from 'react';
-import { Link, NavLink } from 'react-router-dom';
+import { Link, NavLink, useNavigate } from 'react-router-dom';
 
 
 export const Navbar = () => {
+
+    const navigate = useNavigate()
+
+    const onLogout = () => {
+        navigate('/marvel', {
+            replace: true 
+        })
+    }
+
+
     return (
         <nav className="navbar navbar-expand-sm navbar-dark bg-dark p-2">
             
@@ -13,7 +23,7 @@ export const Navbar = () => {
                 Asociaciones
             </Link>
 
-            <div className="navbar-collapse">
+            <div      className="navbar-collapse">
                 <div className="navbar-nav">
 
                     <NavLink 
@@ -40,7 +50,7 @@ export const Navbar = () => {
                    </span>
                    <button
                    className='nav-item nav-link btn'
-                   
+                   onClick={onLogout}
                    >Logout</button>
                 </ul>
             </div>
